df21e36a5fe1be0fa6c6c0f5d5c7a978ef59730c
Wambui <nderituwambui93@gmail.com>
2024-04-19 22:14:56 +0300

    updates to canvas

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    const paragraph = document.querySelector('p#text')
+    paragraph.textContent = 'This is really cool!'
+})
